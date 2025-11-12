cc2c78d987a9bec9be1b1e641ac288f33d06b74f
Sayana <sayanahan05@gmail.com>
2025-11-12 09:39:27 +0500

    Initial Commit

---
+++ MyFavorites/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFavorites
+//
+//  Created by Zhumatay Sayana on 12.11.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
